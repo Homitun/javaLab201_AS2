287a77631fe5fffb8b32795f3da6f485d349019c
Homitun <hoangtuan@gamil.com>
2021-10-04 00:11:24 +0700

    first commit

---
+++ src/dao/InjectionList.java
@@ -0,0 +1,228 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import dto.Injection;
+import dto.Menu;
+import dto.Province;
+import dto.Vaccine;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.text.StyledEditorKit;
+import util.MyToys;
+
+/**
+ *
+ * @author Walter White
+ */
+public class InjectionList {
+
+    StudentList sList = new StudentList();
+    VaccineList vList = new VaccineList();
+    ProvinceList pList = new ProvinceList();
+    ArrayList<Injection> injectionList = new ArrayList<>();
+
+    public InjectionList() {
+    }
+
+    public void setsList(StudentList sList) {
+        this.sList = sList;
+    }
+
+    // Search doctor return pos
+    public int searchInjectionByID(int injectionID) {
+        for (int i = 0; i < injectionList.size(); i++) {
+            if (injectionID == injectionList.get(i).getId()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // add new injection
+    public void AddFirstInjection() throws ParseException {
+        Province a;
+        String i;
+        int pos1, pos, id, vaccineID;
+        String injectionPlace1, studentId, injectionPlace2 = null;
+        Date injectionDate1, injectionDate2 = null;
+        Date today = new Date();
+        Date tomorrow = new Date(today.getTime() + (1000 * 60 * 60 * 24));
+        id = generateID();
+        do {
+            do {
+                studentId = MyToys.getID("Enter student's ID (AAXXXXXX): ",
+                        "The format of ID is AAXXXXXX", "^((SE)|(SS))\\d{6}$");
+                pos = sList.searchStudentByID(studentId);
+
+                if (pos != -1) {
+                    System.out.println("Valid");
+
+                } else {
+                    System.out.println("Student ID is invalid, please re_enter!!");
+                    System.out.println("Student list:");
+                    sList.printStudentList();
+                }
+            } while (pos == -1);
+            do {
+                vaccineID = MyToys.getAnInteger("Enter vaccine id: ", "Vaccine must be interger (1-99)", 1, 99);
+                pos = vList.searchVaccineByID(vaccineID);
+                if (pos != -1) {
+                    System.out.println("Valid");
+                } else {
+                    System.out.println("Vaccine ID is invalid, please re_enter!!");
+                    System.out.println("Vaccine list:");
+                    vList.printVaccineList();
+                }
+            } while (pos == -1);
+        } while (pos == -1);
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        injectionDate1 = MyToys.getADate("Enter injection date (dd-MM-yyyy):",
+                "Wrong valid date range(from 01-01-2021 to today )",
+                df.parse("1-1-2021"), tomorrow);
+        do {
+            injectionPlace1 = pList.searchPlaceByName();
+        } while (injectionPlace1 == null);
+        injectionList.add(new Injection(id, injectionPlace1, injectionPlace2, injectionDate1, injectionDate2, studentId, vaccineID));
+        System.out.println("A student's vaccine injection information is sucessfully added!");
+        writeFile();
+    }
+
+    // tao id ngau nhien
+    private int generateID() {
+        if (injectionList.isEmpty()) {
+            return 1;
+        }
+        Collections.sort(injectionList);
+        return injectionList.get(injectionList.size() - 1).getId() + 1;
+    }
+
+    //show ịnectionList
+    public void showInjectionList() throws ParseException {
+        if (injectionList.isEmpty()) {
+            System.out.println("The list is empty. Nothing to print");
+            return;
+        }
+        for (Injection injection : injectionList) {
+            injection.showProfile();
+            if (injection.getInjectionDate2() != null) {
+                injection.showSecondInjection();
+            } else {
+                System.out.println("Second Vaccination is empty");
+            }
+        }
+    }
+
+    public void deleteInjection() {
+        int pos, id;
+        id = MyToys.getAnInteger("Input id to delete: ", "Injection ID can not be empty,"
+                + " id must be positive interger", 1, Integer.MAX_VALUE);
+        pos = searchInjectionByID(id);
+        if (pos == -1) {
+            System.err.println("Not found");
+        } else {
+            boolean c = false;
+            System.out.println("Are you sure you want to delete?");
+            c = menuYesNo();
+            if (c == true) {
+                injectionList.remove(pos);
+                System.out.println("The Injection information is remove sucessfully");
+            }
+            return;
+        }
+    }
+
+    // input data
+    public void inputData() {
+        sList.readFile();
+        vList.readFile();
+        pList.readFile();
+        readFile();
+    }
+
+    public Injection checkInjectionByStudentid(String studentID) {
+        for (Injection injection : injectionList) {
+            if (studentID.equalsIgnoreCase(injection.getStudentId())) {
+                return injection;
+            }
+        }
+        return null;
+    }
+
+    public void writeFile() {
+        try {
+            FileWriter fw = new FileWriter("injection.txt", true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (Injection injection : injectionList) {
+                bw.write(injection.toString());
+            }
+            bw.close();
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+    public ArrayList<Injection> readFile() {
+        DateFormat formatter = new SimpleDateFormat("MM/dd/yy h:mm a");
+
+        try {
+            FileReader fr = new FileReader("injection.txt");
+
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            while (true) {
+                line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                String[] txt = line.split(";");
+                int id = Integer.parseInt(txt[0]);
+                String studentId = txt[1];
+                int vaccineID = Integer.parseInt(txt[2]);
+                String injectionPlace1 = txt[3];
+                Date injectionDate1 = (Date) formatter.parse(txt[4]);
+                String injectionPlace2 = txt[5];
+                Date injectionDate2 = (Date) formatter.parse(txt[6]);
+                injectionList.add(new Injection(id, injectionPlace1, injectionPlace2, injectionDate1, injectionDate2, studentId, vaccineID));
+            }
+
+        } catch (Exception e) {
+        }
+        return injectionList;
+    }
+    public boolean menuYesNo() {
+        boolean flat = true;
+        while (flat) {
+            System.out.println("1.Yes");
+            System.out.println("2.No");
+            int choose;
+            do {
+
+                choose = MyToys.getAnInteger("Enter your choose:",
+                        "Choice must be interger! ");
+            } while (choose < 0 || choose > 2);
+            switch (choose) {
+                case 1: {
+                    return true;
+                }
+                case 2: {
+                    return false;
+                }
+            }
+        }
+        return flat;
+    }
+
+}
---
+++ src/gui/Program.java
@@ -0,0 +1,89 @@
+package gui;
+
+import dao.InjectionList;
+import dao.StudentList;
+import dao.VaccineList;
+import dto.Menu;
+import java.text.ParseException;
+import util.MyToys;
+
+/**
+ *
+ * @author Walter White
+ */
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+        InjectionList injecList = new InjectionList();
+
+        // doctor menu
+        Menu doctorMenu = new Menu("Welcome to Covid-19 Vaccine Management-@2021 by <SE151471-Hoang Minh Tuan>");
+        doctorMenu.addNewOption("1.Show information all students have been injected");
+        doctorMenu.addNewOption("2.Add student's vaccine injection information");
+        doctorMenu.addNewOption("3.Updating information of students' vaccine injection");
+        doctorMenu.addNewOption("4.Delete student vaccine injection information");
+        doctorMenu.addNewOption("5.Search for injection information by studentID");
+        doctorMenu.addNewOption("6.Others - Quit");
+
+        int choice;
+        do {
+            injecList.inputData();
+            doctorMenu.printMenu();
+            choice = doctorMenu.getChoice();
+            switch (choice) {
+                case 1: {
+                    System.out.println("=========Show information all students have been injected=======");
+                    injecList.showInjectionList();
+                    break;
+                }
+                case 2: {
+                    boolean c = true;// c sand for continue
+                    while (c) {
+                        System.out.println("=========Add student's vaccine injection information=======");
+                        injecList.AddFirstInjection();
+                        System.out.println("Do you want to continue Add ?");
+                        c = injecList.menuYesNo();
+                    }
+                    break;
+                }
+                case 3: {
+                    boolean c = true;// c sand for continue
+                    while (c) {
+                        System.out.println("=========Updating information of students' vaccine injection=======");
+                        
+                        c = injecList.menuYesNo();
+                    
+                    }
+                    break;
+                }
+                case 4: {
+                    boolean c = true;// c sand for continue
+                    while (c) {
+                        System.out.println("========Delete student vaccine injection information=======");
+                        injecList.deleteInjection();
+                        c = injecList.menuYesNo();
+                    }
+                    break;
+                }
+                case 5: {
+                    boolean c = true;// c sand for continue
+                    while (c) {
+                        System.out.println("========Search for injection information by studentID========");
+                        String studentID = MyToys.getID("Enter student's ID (AAXXXXXX): ",
+                                "The format of ID is AAXXXXXX", "^((SE)|(SS))\\d{6}$");
+                        injecList.checkInjectionByStudentid(studentID).showProfile();
+                        c = injecList.menuYesNo();
+                    }
+                    break;
+                }
+                case 6: {
+                    System.out.println("=========Others - Quit=======");
+                    System.out.println("See you, enjoy!");
+                    break;
+                }
+            }
+        } while (choice != 6);
+
+    }
+
+}
